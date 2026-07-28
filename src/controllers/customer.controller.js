c45433d5c75091731f707fda24e43b33fd74ec54
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-28 14:17:56 +0100

    created the register customer function in the controller

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -1,10 +1,11 @@
 const customerService = require("../services/customer.service");
+const asyncHandler = require("../utils/asyncHandler");
 
-exports.createCustomer = async(req, res) => {
-    const customer = await customerService.createCustomer(req.body);
+exports.registerCustomer = asyncHandler(async(req, res) => {
+    const customer = await customerService.registerCustomer(req.body);
 
     res.status(201).json({
         message: "Customer created successfully",
         data: customer
     });
-}
+});
