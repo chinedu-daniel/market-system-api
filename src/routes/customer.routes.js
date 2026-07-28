e279fa42777ed1be093621fe98900e3717c8837b
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-28 15:12:19 +0100

    added the endpoint for creating a customer

---
+++ src/routes/customer.routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const customerController = require("../controllers/customer.controller");
+
+const protect = require("../middleware/auth.middleware");
+const authorize = require("../middleware/authorize.middleware");
+
+const validate = require("../middleware/validate");
+const { registerCustomerSchema } = require("../validators/customer.schema");
+
+router.post(
+    "/",
+    protect,
+    authorize("admin", "sales"),
+    validate(registerCustomerSchema),
+    customerController.registerCustomer
+);
