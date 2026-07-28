7965f0f0afe2ab6b26f1393ed26c12ebf2eb5c9e
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-28 16:30:47 +0100

    forgot to import the customer routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const errorMiddleware = require("./middleware/error.middleware");
 const userRoutes = require("./routes/user.routes");
+const customerRoutes = require("./routes/customer.routes");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 // Route
 app.use("/api/users", userRoutes);
+app.use("/api/customers", customerRoutes);
 
 // Global error handler
 app.use(errorMiddleware);
