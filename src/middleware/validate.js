47fc5488ab3c5a90d2f6f9bfcbbfca2f57feffc1
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-28 15:23:59 +0100

    removed unused codes

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -1,15 +1,9 @@
 function validate(schema) {
-
     return function(req, res, next) {
         const { error } = schema.validate(req.body);
         
         if (error) {
-            // return res.status(400).json({
-            //     message: error.details[0].message
-            // });
-
             const error = new Error("Email is required");
-
             return next(error);
         }
         
