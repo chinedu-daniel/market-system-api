bc33e011aea6052474c1904f7d2de712f2aedac5
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-28 15:10:07 +0100

    added req.user as we only want authenticated/registered users to access or register customers

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -2,7 +2,7 @@ const customerService = require("../services/customer.service");
 const asyncHandler = require("../utils/asyncHandler");
 
 exports.registerCustomer = asyncHandler(async(req, res) => {
-    const customer = await customerService.registerCustomer(req.body);
+    const customer = await customerService.registerCustomer(req.body, req.user);
 
     res.status(201).json({
         message: "Customer created successfully",
