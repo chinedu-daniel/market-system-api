de06065fbf52a921de72e3ebb053f3c409c755be
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-28 17:08:48 +0100

    testing the server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,34 @@
 require("dotenv").config();
 
+// console.log({
+//     DB_HOST: process.env.DB_HOST,
+//     DB_PORT: process.env.DB_PORT,
+//     DB_NAME: process.env.DB_NAME,
+//     DB_USER: process.env.DB_USER,
+//     DB_PASSWORD_TYPE: typeof process.env.DB_PASSWORD,
+//     DB_PASSWORD_EXISTS: !!process.env.DB_PASSWORD,
+// });
+
 const app = require("./app");
+const db = require("./db/database");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-});
+async function startServer() {
+    try{
+        await db.query("SELECT NOW()");
+
+        console.log("Database connected successfully");
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        });
+    } catch(error) {
+        console.error("Failed to connect to database");
+        console.error(error.message);
+
+        process.exit(1);
+    }
+}
+
+startServer();
