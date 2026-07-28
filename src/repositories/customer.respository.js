1fda2bc5c3f6646cd191848cadc11d39fcbea0a6
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-28 11:59:26 +0100

    added the find customer by email and create customer

--- src/repositories/customer.respository.js
+++ src/repositories/customer.respository.js
@@ -3,22 +3,46 @@ const db = require("../db/database");
 exports.findCustomerByEmail = async (email) => {
     const result = await db.query(
         `
-        SELECT 
-        `
+        SELECT *
+        FROM customers
+        WHERE email = $1
+        `,
+        [email]
     );
-}
-
-exports.createCustomer = async(customerData) => {
-    const query = `
-        INSERT INTO customers
-        (first_name,last_name,email,phone)
-        VALUES($1,$2,$3,$4)
-        RETURNING *
-    `;
-
-    const values = Object.values(customer);
-
-    const result = await db.query(query, values);
 
     return result.rows[0];
 }
+
+exports.createCustomer = async(customerData) => {
+
+    const {
+        first_name,
+        last_name,
+        email,
+        phone
+    } = customerData;
+
+    const result = await db.query(
+        `
+        INSERT INTO customers
+        (
+            first_name,
+            last_name,
+            email,
+            phone
+        )
+
+        VALUES($1,$2,$3,$4)
+
+        RETURNING *
+        `,
+        [
+            first_name,
+            last_name,
+            email,
+            phone
+        ]
+    );
+
+    return result.rows[0];
+};
