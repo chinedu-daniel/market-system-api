69e7a6d76cd8aa468eb3e1050762f01814ba25d2
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-28 17:45:11 +0100

    renamed the file name to repository from respostory

---
+++ src/repositories/customer.repository.js
@@ -0,0 +1,48 @@
+const db = require("../db/database");
+
+exports.findCustomerByEmail = async (email) => {
+    const result = await db.query(
+        `
+        SELECT *
+        FROM customers
+        WHERE email = $1
+        `,
+        [email]
+    );
+
+    return result.rows[0];
+}
+
+exports.createCustomer = async(customerData) => {
+
+    const {
+        first_name,
+        last_name,
+        email,
+        phone
+    } = customerData;
+
+    const result = await db.query(
+        `
+        INSERT INTO customers
+        (
+            first_name,
+            last_name,
+            email,
+            phone
+        )
+
+        VALUES($1,$2,$3,$4)
+
+        RETURNING *
+        `,
+        [
+            first_name,
+            last_name,
+            email,
+            phone
+        ]
+    );
+
+    return result.rows[0];
+};
