29bb6173aab4db953cd17bcb6fd8cd3c1786ccc9
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-29 15:37:34 +0100

    database

--- src/server.js
+++ src/server.js
@@ -1,14 +1,5 @@
 require("dotenv").config();
 
-// console.log({
-//     DB_HOST: process.env.DB_HOST,
-//     DB_PORT: process.env.DB_PORT,
-//     DB_NAME: process.env.DB_NAME,
-//     DB_USER: process.env.DB_USER,
-//     DB_PASSWORD_TYPE: typeof process.env.DB_PASSWORD,
-//     DB_PASSWORD_EXISTS: !!process.env.DB_PASSWORD,
-// });
-
 const app = require("./app");
 const db = require("./db/database");
 
