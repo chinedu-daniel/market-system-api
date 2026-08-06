875938f7bcb0b90112a78f0e2cfe71e37d96c32b
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-08-06 21:35:34 +0100

    get the find all orders function

--- src/modules/orders/order.repository.js
+++ src/modules/orders/order.repository.js
@@ -15,3 +15,24 @@ exports.createOrder = async (customerId, totalAmount) => {
 
     return result.rows[0];
 };
+
+exports.findAllOrders = async () => {
+    const result = await db.query(
+        `
+        SELECT 
+            o.id,
+            c.id AS customer_id,
+            c.first_name,
+            c.last_name,
+            c.email,
+            o.total_amount,
+            o.created_at
+        FROM orders o
+        INNER JOIN customers c
+            ON o.customer_id = c.id
+        ORDER BY o.created_at DESC
+        `
+    );
+    
+    return result.rows;
+};
