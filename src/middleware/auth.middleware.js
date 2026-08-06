2114da83c485b85a0eef1562ee7c8506dd6d4b5e
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-08-06 21:30:37 +0100

    added the secret key to the env

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -15,7 +15,7 @@ function protect(req, res, next) {
     const token = authHeader.split(" ")[1];
 
     try {
-        const decoded = jwt.verify(token, "my_super_secret_key");
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         req.user = decoded;
 
