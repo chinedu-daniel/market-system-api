58612d08b6b74a4538023c22dc83beb7e334afe4
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-08-06 13:50:28 +0100

    the order route

---
+++ src/modules/orders/order.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const orderController = require("./order.controller");
+
+router.post(
+    "/", 
+    orderController.createOrder
+);
+
+module.exports = router;
