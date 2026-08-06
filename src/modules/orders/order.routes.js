4f464bfb450483768d5baf483ee02225d4554215
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-08-06 21:36:28 +0100

    get order endpoint

--- src/modules/orders/order.routes.js
+++ src/modules/orders/order.routes.js
@@ -2,10 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const orderController = require("./order.controller");
+const protect = require("../../middleware/auth.middleware");
+const authorize = require("../../middleware/authorize.middleware");
 
 router.post(
-    "/", 
+    "/",
+    protect,
+    authorize("admin", "staff"),
     orderController.createOrder
+);
+
+router.get(
+    "/",
+    protect,
+    authorize("admin", "staff"),
+    orderController.getAllOrders
 );
 
 module.exports = router;
