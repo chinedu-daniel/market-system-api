5b659a7f33350dc5e80cc8cc67ec9786af675aae
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-08-07 17:24:27 +0100

    the business/application logic for ALL and BY ID

--- src/modules/orders/order.service.js
+++ src/modules/orders/order.service.js
@@ -1,5 +1,6 @@
 const AppError = require('../../utils/appError');
 const customerRepository = require('../customers/customer.repository');
+const { toOrderResponse } = require('./dto/order-response.dto');
 const orderRepository = require('./order.repository');
 
 exports.createOrder = async(customerId, totalAmount) => {
@@ -11,9 +12,21 @@ exports.createOrder = async(customerId, totalAmount) => {
 
     const order = await orderRepository.createOrder(customerId, totalAmount);
 
-    return order;
+    return toOrderResponse(order);
 };
 
 exports.getAllOrders = async() => {
-    return await orderRepository.findAllOrders();
+    const order = await orderRepository.findAllOrders();
+
+    return order.map(toOrderResponse);
 };
+
+exports.getOrderById = async (id) => {
+    const order = await orderRepository.findOrderById(id)
+
+    if (!order) {
+        throw new AppError("Order not found", 404);
+    }
+
+    return toOrderResponse(order);
+}
