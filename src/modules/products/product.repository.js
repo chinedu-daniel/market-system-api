a31031a3c3d0574bb823e44eb910187becde97c9
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-08-08 06:27:44 +0100

    added the createProduct in the repository

---
+++ src/modules/products/product.repository.js
@@ -0,0 +1,31 @@
+const db = require("../../db/database");
+
+exports.createProduct = async ({ 
+    name,
+    description,
+    price,
+    quantity
+}) => {
+    const result = await db.query(
+        `
+        INSERT INTO products (
+            name,
+            description,
+            price,
+            quantity
+        )
+        VALUES ($1, $2, $3, $4)
+        RETURNING 
+            id,
+            name,  
+            description,
+            price,
+            quantity,
+            created_at,
+            updated_at 
+       `,
+       [name, description, price, quantity]    
+    );
+
+    return result.rows[0];
+};
