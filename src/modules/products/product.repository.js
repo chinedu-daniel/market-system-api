48faf2a0233efd234fae48c477eaf05fc5339aac
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-08-08 11:51:52 +0100

    the database schema

--- src/modules/products/product.repository.js
+++ src/modules/products/product.repository.js
@@ -1,5 +1,25 @@
 const db = require("../../db/database");
 
+exports.findProductByName = async (name) => {
+    const result = await db.query(
+        `
+        SELECT
+            id,
+            name,
+            description,
+            price,
+            quantity,
+            created_at,
+            updated_at
+        FROM products
+        WHERE name = $1
+        `,
+        [name]
+    );
+
+    return result.rows[0];
+};
+
 exports.createProduct = async ({ 
     name,
     description,
