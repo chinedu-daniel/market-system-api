31479704d5bca068a9d6b1541327d01062312187
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-08-08 11:56:18 +0100

    mounting the product route

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const errorMiddleware = require("./middleware/error.middleware");
 const { userRoutes } = require("./modules/users");
 const { customerRoutes } = require("./modules/customers");
 const { orderRoutes } = require("./modules/orders");
+const { productRoutes } = require("./modules/products");
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/customers", customerRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/products", productRoutes)
 
 // Global error handler
 app.use(errorMiddleware);
