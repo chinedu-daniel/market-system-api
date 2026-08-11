9112d6b69e347228ec9acba322a5627644b215d9
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-08-11 10:39:37 +0100

    the product update schema

--- src/modules/products/product.validator.js
+++ src/modules/products/product.validator.js
@@ -7,6 +7,14 @@ const productIdSchema = Joi.object({
         .required()
 });
 
+const productUpdateSchema = Joi.object({
+    name: Joi.string().trim().min(1),
+    description: Joi.string().trim().min(1),
+    price: Joi.number().positive(),
+    quantity: Joi.number().integer().min(0)
+}).min(1);
+
 module.exports = {
-    productIdSchema
+    productIdSchema,
+    productUpdateSchema
 };
