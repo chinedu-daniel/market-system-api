e50bb2e1b885100b633287a68d1f6c38851f940b
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-08-13 17:27:36 +0100

    the order item for transaction

---
+++ src/modules/orders/orderItem.repository.js
@@ -0,0 +1,35 @@
+exports.createOrderItem = async ({
+    client,
+    orderId,
+    productId,
+    quantity,
+    unitPrice
+}) => {
+    const result = await client.query(
+        `
+        INSER INTO order_items
+        (
+            order_id,
+            product_id,
+            quantity,
+            unit_price
+        )
+        VALUES ($1, $2, $3, $4)
+        RETURNING
+            id,
+            order_id,
+            product_id,
+            quantity,
+            unit_price,
+            created_at
+        `,
+        [
+            orderId,
+            productId,
+            quantity,
+            unitPrice
+        ]
+    );
+
+    return result.rows[0];
+};
