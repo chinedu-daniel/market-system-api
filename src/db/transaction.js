2f6256efd344f740a5ee3c4cd643fab931391425
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-08-14 13:04:55 +0100

    the client for our connection in the transaction

---
+++ src/db/transaction.js
@@ -0,0 +1,23 @@
+const db = require("./database");
+
+const withTransaction = async (callback) => {
+    const client = await db.connect();
+
+    try {
+        await client.query("BEGIN");
+
+        const result = await callback(client);
+
+        await client.query("COMMIT");
+
+        return result;
+    } catch (error) {
+        await client.query("ROLLBACK");
+
+        throw error;
+    } finally {
+        client.release();
+    }
+};
+
+module.exports = withTransaction;
