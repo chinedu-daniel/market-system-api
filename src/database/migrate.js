6de94ef9b2d26d706dc3cdfb1477aa8907b02261
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-08-21 15:18:09 +0100

    file system to make the migration read or locate the sql files

--- src/database/migrate.js
+++ src/database/migrate.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const path = require("path");
+const db = require("../db/database");
+// const { error } = require("console");
 
 const migrationsDir = path.join(__dirname, "migrations");
 
@@ -8,4 +10,18 @@ const files = fs
     .filter((file) => file.endsWith(".sql"))
     .sort();
 
-console.log(files);
+// console.log(files);
+
+async function getExecutedMigrations() {
+    const result = await db.query("SELECT filename FROM schema_migrations ORDER BY id");
+
+    return result.rows.map((row) => row.filename);
+}
+
+getExecutedMigrations()
+    .then((migrations) => {
+        console.log(migrations);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
