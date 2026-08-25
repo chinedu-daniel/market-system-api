c022fac4c74968fa0bfb28fc6a2642cb97c00093
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-08-25 09:42:15 +0100

    removed the loading the env config from the config file

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,9 +1,3 @@
-require("dotenv").config();
-
 module.exports = {
-//     appUrl:
-//         process.env.APP.URL || 
-//         "http://localhost:3000",
-
 frontendUrl: process.env.FRONTEND_URL
 };
