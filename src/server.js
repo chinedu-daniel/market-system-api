696602b3af305ce9a9adfc2201ebdee5062a88c0
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-09-04 11:11:12 +0100

    the cleanup session starting up in the server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 require("dotenv").config();
 
+const cleanupExpiredSessions = require("./jobs/sessionCleanup");
+
 const app = require("./app");
 const db = require("./db/database");
 
 const PORT = process.env.PORT || 3000;
+
+cleanupExpiredSessions(); // Run the session cleanup job on server start
+
+setInterval(cleanupExpiredSessions,
+    60 * 60 * 1000
+);
 
 async function startServer() {
     try{
