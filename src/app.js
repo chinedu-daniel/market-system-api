8ee8b1824f13c76d91402c5493644df6e058710c
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-09-12 06:42:13 +0100

    imported cors for cross site origin test, helmet for the http header, morgan for logging

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-// const cors = require("cors");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
 const errorMiddleware = require("./middleware/error.middleware");
 const { userRoutes } = require("./modules/users");
 const { customerRoutes } = require("./modules/customers");
@@ -9,15 +11,18 @@ const { productRoutes } = require("./modules/products");
 
 const app = express();
 
-// app.use(
-//     cors({
-//         origin: "http://localhost:5173"
-//     })
-// );
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true
+    })
+);
 
 // Built-in middleware
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
+app.use(helmet());
+app.use(morgan("combined"));
 
 // Route
 app.use("/api/users", userRoutes);
