82931b7095dec35593623a224a50da6c1c81ced1
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-09-22 14:40:32 +0100

    test if all product, product id and update product work as was stated

--- src/test/unit/products/product.service.test.js
+++ src/test/unit/products/product.service.test.js
@@ -14,6 +14,8 @@ const mockProduct = {
 jest.spyOn(productRepository, "findProductById")
     .mockResolvedValue(mockProduct);
 
+jest.spyOn(productRepository, "updateProduct");
+
 test("returns product when product exists", async () => {
     await expect(
         productService.getProductById(1)
@@ -39,4 +41,42 @@ test("returns 404 when product is not found", async () => {
     } catch (error) {
         expect(error.statusCode).toBe(404);
     }
+});
+
+test("updates product when product exists", async () => {
+    const existingProduct = {
+        id: 1,
+        name: "Suzuki K6A",
+        price: 500000.00,
+        quantity: 10,
+        is_active: true
+    };
+
+    const updatedProduct = {
+        id: 1,
+        name: "Suzuki K6A",
+        price: 550000.00,
+        quantity: 8,
+        is_active: true
+    };
+
+    productRepository.findProductById.mockResolvedValue(existingProduct);
+
+    productRepository.updateProduct.mockResolvedValue(updatedProduct);
+
+    const result = await productService.updateProduct(1, {
+        price: 550000.00,
+        quantity: 8
+    });
+
+    await expect(result).toEqual({
+        id: 1,
+        name: "Suzuki K6A",
+        description: undefined,
+        price: 550000,
+        quantity: 8,
+        isActive: true,
+        createdAt: undefined,
+        updatedAt: undefined
+    });
 });
